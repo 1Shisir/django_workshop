dc678ca1d5f73c14d862e1fc3c0f54f0686928d8
1Shisir <87371639+1Shisir@users.noreply.github.com>
2024-08-13 11:57:51 +0545

    error solved:student table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    roll = models.IntegerField()
+    phone = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    faculty_choices = (
+        ('Management','Management'),
+        ('Science & Technology','Science & Technology'),
+    )
+    marks = models.IntegerField()
+    programme_choices = (
+        ('BBA','BBA'),
+        ('BCA','BCA'),
+        ('MCA','MCA'),
+        ('MBA','MBA'),
+    )
+    faculty = models.CharField(max_length=50, choices=faculty_choices)
+    programme = models.CharField(max_length=20, choices=programme_choices)
+
+
+    def __str__(self):
+        return f'{self.name}- {self.phone}'
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,8 @@ def about(request):
     return HttpResponse('About Page')
 
 def contacts(request):
-    return HttpResponse('Contact Us Page')        
+    return HttpResponse('Contact Us Page')    
+
+def read(request):
+    students = Student.objects.all()
+    return render(request, 'read.html', {'students':students})
